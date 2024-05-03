7a743c9c7dac0fed4d43f2206f245cf4996f81fc
Daniel-Aaron-Bloom <76709210+Daniel-Aaron-Bloom@users.noreply.github.com>
2024-05-03 01:11:21 -0700

    docs: update wording and examples in docs (#2)
    * docs: update docs

--- src/lib.rs
+++ src/lib.rs
@@ -2,23 +2,38 @@
 //!
 //! ## Description
 //!
-//! Sometimes a series of tests are progressive/incremental; One test is
-//! targetted at verifying Step 1 works as expected, while another is focused
-//! on ensuring Step 2 functions correctly. Common advice is to duplicate all
-//! the work of Step 1 into Step 2's test, or to combine the tests into one
-//! large test. However the former approach can significantly slow down tests,
-//! while the latter can lead to large and unruly testing functions which are
-//! difficult to maintain.
+//! Sometimes a series of tests are progressive or incremental; that is to say
+//! one test builds on another. A multi-stage test might have complicated
+//! setup and verification processes for each step, but with clear boundaries
+//! between stages (`test_1` verifies stage 1, `test_2` verifies stage 2, etc.
+//! ). The problem arises when stages want to share data (i.e. `test_2` wants
+//! to start where `test_1` left off).
 //!
-//! This crate takes a different approach by allowing a test to return a
-//! fixture which can be used in subsequent tests, all through the use of a
-//! single attribute macro [`tested_fixture`].
+//! Common advice is to duplicate all the setup code across all tests, or
+//! alternatively to combine the tests into one large test. However the former
+//! approach can significantly slow down tests if setup is costly, and also
+//! introduces significant test maintenance costs if setup procedures change.
+//! The latter however can lead to large and unruly testing functions which are
+//! difficult to maintain, and doesn't solve the problem when dependencies
+//! cross multiple files (i.e. unit tests which test the full setup process for a
+//! `Foo` are difficult to combine with unit tests which test the setup process
+//! of a `Bar` which relies on a fully constructed `Foo`; should the "combined"
+//! test live near `Foo` or `Bar`? What if the tests needs to access internals to
+//! verify assertions?).
+//!
+//! This crate provides an alternative approach by allowing a test to return a
+//! fixture which can be used in subsequent tests. Tests can opt in to this
+//! functionality by using a single attribute macro [`tested_fixture`].
 //!
 //! ## Usage
 //!
 //! When writing tests for code like:
 //! ```
 //! struct Foo {
+//!     // ...
+//! }
+//!
+//! struct State {
 //!     // ...
 //! }
 //!
@@ -29,7 +44,13 @@
 //!         }
 //!     }
 //!
-//!     fn step_2(&self) {
+//!     fn step_2(&self) -> State {
+//!         State {
+//!             // Complicated execution...
+//!         }
+//!     }
+//!
+//!     fn step_3(&self, v: &State) {
 //!         // Complicated execution...
 //!     }
 //! }
@@ -51,10 +72,22 @@
 //!     foo.step_2();
 //!     // Complicated assertions verify step 2...
 //! }
+//!
+//! #[test]
+//! fn step_3() {
+//!     let foo = Foo::step_1();
+//!     // (Some?) Complicated assertions verify step 1...
+//!
+//!     let state = foo.step_2();
+//!     // (Some?) Complicated assertions verify step 2...
+//!
+//!     foo.step_3(&state);
+//!     // Complicated assertions verify step 3...
+//! }
 //! ```
 //!
 //! As you can see, with a lot of steps, this can quickly get out of hand. To
-//! clean it up is straightforward by switching `step_1` to use the
+//! clean it up is straightforward by switching to use the
 //! `tested_fixture` attribute instead of the normal `test`.
 //!
 //! ```
@@ -66,18 +99,25 @@
 //!     foo
 //! }
 //!
-//! #[test]
-//! fn step_2() {
-//!     STEP_1.step_2();
+//! #[tested_fixture::tested_fixture(STEP_2_STATE)]
+//! fn step_2() -> State {
+//!     let state = STEP_1.step_2();
 //!     // Complicated assertions verify step 2...
+//!     state
+//! }
+//!
+//! #[test]
+//! fn step_3() {
+//!     STEP_1.step_3(&STEP_2_STATE);
+//!     // Complicated assertions verify step 3...
 //! }
 //! ```
 //!
-//! In the case where only `step_2` is run, `STEP_1` will be initialized on
+//! Note that when only `step_2` is run, `STEP_1` will be initialized on
 //! first access. Since the order of tests is not guaranteed, this actually can
 //! occur even if both tests are run. But since results are cached, the
-//! `step_1` test should still reproduce the same result regardless of if it is
-//! the first access or not.
+//! `step_1` test should still succeed (or fail) regardless of if it is run
+//! first or not.
 //!
 //! ## Advanced usage
 //!
@@ -87,7 +127,10 @@
 //! only `Ok` return values should be captured. For example:
 //!
 //! ```
-//! #[tested_fixture::tested_fixture(pub(crate) STEP_1: Foo)]
+//! #[tested_fixture::tested_fixture(
+//!     /// Doc comment on the `STEP_1` global variable
+//!     pub(crate) STEP_1: Foo
+//! )]
 //! fn step_1() -> Result<Foo, &'static str> {
 //!     // ...
 //! }
@@ -95,15 +138,22 @@
 //!
 //! ## Limitations
 //!
-//! Ordinary test are able to return anything which implements
-//! [`std::process::Termination`], which unlimited nestings of `Result`s. While
-//! this crate does support returning nested `Result` wrappings, it only does
-//! so up to a fixed depth. Additionally it does not support returning any other
-//! types of `Termination`
+//! Ordinary `#[test]` functions are able to return anything which implements
+//! [`std::process::Termination`], including unlimited nestings of `Result`s.
+//! While this crate does support returning nested `Result` wrappings, it only
+//! does so up to a fixed depth. Additionally it does not support returning any
+//! other `Termination` implementations besides `Result`.
+//!
+//! As with all testing-related global state, it is recommended that tests don't
+//! mutate the state, as doing so will increase the risk of flaky tests due to
+//! changes in execution order or timing. Thankfully this is the default
+//! behavior, as all fixtures defined by this crate are only accessible by
+//! non-mutable reference.
 //!
 //! Right now this crate does not support async tests.
 
 #![warn(missing_docs)]
+#![allow(clippy::test_attr_in_doctest)]
 
 pub use tested_fixture_macros::tested_fixture;
 
@@ -173,7 +223,6 @@ pub mod helpers {
         }
     }
 
-
     /// Helper trait for unwrapping fixtures
     pub trait Unwrap<T>: Termination {
         fn unwrap(self, context: &str) -> &'static T;
@@ -237,6 +286,14 @@ mod tests {
         fn build(v: u32) -> Self {
             HeavySetup(v)
         }
+    }
+
+    #[tested_fixture(
+        /// This is a test fixture
+        pub(crate) SETUP_0
+    )]
+    fn setup_with_attributes() -> HeavySetup {
+        HeavySetup::build(1)
     }
 
     #[tested_fixture(SETUP_1)]
